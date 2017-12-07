e1624557207eb5760533a29d4b4fb88b7cca905b
nutanek <nutcs@hotmail.com>
2017-12-07 20:11:43 +0700

    edit controller

--- controller.rb
+++ controller.rb
@@ -17,28 +17,28 @@ class SortController
     end
     
     def getNums
-		@view.renderSortInfo(@model.getSortedNums())
-	end
+        @view.renderSortInfo(@model.getSortedNums())
+    end
 
-	def sortArray(unSortedArr)
-		arrNum = unSortedArr
-		lenNums = arrNum.length
-		arrNum.each_with_index do |num1, index|
-			min = num1
-			count = index+1
-			pos = index
-			arrNum[index+1, lenNums].each do |num2|
-				if num2 < min
-					min = num2
-					pos = count
-				end
-				count+=1
-			end
-			arrNum[index],arrNum[pos] = arrNum[pos],arrNum[index]
-		end
-		sortedArr = arrNum
-		return sortedArr
-	end
-
-	private :sortArray
+    def sortArray(unSortedArr)
+        arrNum = unSortedArr
+        lenNums = arrNum.length
+        arrNum.each_with_index do |num1, index|
+            min = num1
+            count = index+1
+            pos = index
+            arrNum[index+1, lenNums].each do |num2|
+                if num2 < min
+                    min = num2
+                    pos = count
+                end
+                count+=1
+            end
+            arrNum[index],arrNum[pos] = arrNum[pos],arrNum[index]
+        end
+        sortedArr = arrNum
+        return sortedArr
+    end
+    
+    private :sortArray
 end
